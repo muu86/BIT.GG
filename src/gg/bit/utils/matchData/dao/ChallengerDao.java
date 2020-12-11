270f58037294652c2c71d6bb6a7d1e02d4d96377
kmj <mu86@naver.com>
2020-12-11 13:51:34 +0900

    init

---
+++ src/gg/bit/utils/matchData/dao/ChallengerDao.java
@@ -0,0 +1,11 @@
+package gg.bit.utils.matchData.dao;
+
+import java.util.List;
+
+import gg.bit.utlils.matchData.vo.ChallengerVo;
+
+public interface ChallengerDao {
+	
+	public List<ChallengerVo> getList();
+	
+}
